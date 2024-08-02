8a3de011ed368eb9208f15a58865de1c5c92e389
Ayush Saini <saini712@live.com>
2024-08-03 00:33:49 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "karatuno",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/karatuno-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "ML",
     bio: "",
     email: "findkaratuno@gmail.com",
